9b2e622bdcbc0925f3fab2553e550fc1cb5633ce
BigSuggs72 <msofi72@gmail.com>
2022-09-13 20:53:40 -0400

    update

--- routes/exercise.js
+++ routes/exercise.js
@@ -14,18 +14,3 @@ router.put('/markIncomplete', exercisesController.markIncomplete)
 router.delete('/deleteTodo', exercisesController.deleteTodo)
 
 module.exports = router
-
-
-
-
-
-
-// router.get('/', ensureAuth, todosController.getTodos)
-
-// router.post('/createTodo', todosController.createTodo)
-
-// router.put('/markComplete', todosController.markComplete)
-
-// router.put('/markIncomplete', todosController.markIncomplete)
-
-// router.delete('/deleteTodo', todosController.deleteTodo)
