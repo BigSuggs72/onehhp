86d32f83651e6f0890816e8bda9aa23836c3b4bb
BigSuggs72 <msofi72@gmail.com>
2022-09-24 09:17:43 -0400

    update

--- routes/exercise.js
+++ routes/exercise.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const upload = require("../middleware/multer");
 const exerciseController = require('../controllers/exercise') 
 const { ensureAuth } = require('../middleware/auth')
 
 router.get('/', ensureAuth, exerciseController.getExercise)
 
-router.post('/createExercise', exerciseController.createExercise)
+router.post('/createExercise', upload.single("file"), exerciseController.createExercise)
 
 // router.put('/markComplete', exerciseController.markComplete)
 
